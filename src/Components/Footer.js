abd3d326a0041ff5bb3e8af9db86c4557aece3c0
omsahare08 <omsahare0608@gmail.com>
2026-02-26 14:49:47 +0530

    fix: add rel attributes to external links in Footer for eslint

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,9 +9,9 @@ const Footer = () => {
       <h4>Developed by SWS</h4>
       <h4>Copyright &copy; 2026</h4>
       <div className='footerLinks'>
-        <a href="https://github.com/sanchitsatpaise" target='_blank'><FaGithub/></a>
-        <a href="https://www.linkedin.com/in/sanchit-satpaise/" target='_blank'><FaLinkedin/></a>
-        <a href='mailto:sanchitsatpaise4771@gmail.com' target='_blank'><GrMail/></a>
+        <a href="https://github.com/sanchitsatpaise" target='_blank' rel="noopener noreferrer"><FaGithub/></a>
+        <a href="https://www.linkedin.com/in/sanchit-satpaise/" target='_blank' rel="noopener noreferrer"><FaLinkedin/></a>
+        <a href='mailto:sanchitsatpaise4771@gmail.com' target='_blank' rel="noopener noreferrer"><GrMail/></a>
       </div>
     </footer>
   )
